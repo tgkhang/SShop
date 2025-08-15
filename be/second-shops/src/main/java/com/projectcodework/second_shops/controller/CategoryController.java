49a2e6d4e68c8c348482d1eeda5ecd970c619ff2
Khang <tgkhang22@clc.fitus.edu.vn>
2025-08-15 19:10:36 +0700

    [+] Category Controller

--- be/second-shops/src/main/java/com/projectcodework/second_shops/controller/CategoryController.java
+++ be/second-shops/src/main/java/com/projectcodework/second_shops/controller/CategoryController.java
@@ -1,4 +1,93 @@
 package com.projectcodework.second_shops.controller;
 
+
+import com.projectcodework.second_shops.exceptions.AlreadyExistsException;
+import com.projectcodework.second_shops.exceptions.ResourceNotFoundException;
+import com.projectcodework.second_shops.model.Category;
+import com.projectcodework.second_shops.response.APIResponse;
+import com.projectcodework.second_shops.service.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    private final ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<APIResponse> getAllCategories() {
+        try
+        {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new APIResponse("Success", categories));
+        }
+        catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new APIResponse("Error", e.getMessage()));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<APIResponse> addCategory(@RequestBody Category categoryName) {
+        try
+        {
+           Category newCategory = categoryService.addCategory(categoryName);
+           return ResponseEntity.ok(new APIResponse("Success", newCategory));
+        }
+        catch(AlreadyExistsException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new APIResponse(e.getMessage(),null));
+        }
+    }
+
+    @GetMapping("/category/{id}/category")
+    public ResponseEntity<APIResponse> getCategoryById(@PathVariable Long id){
+        try
+        {
+            Category category = categoryService.getCategoryById(id);
+            return ResponseEntity.ok(new APIResponse("Success", category));
+        }
+        catch(ResourceNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse(e.getMessage(),null));
+        }
+    }
+
+    @GetMapping("/category/{name}/category")
+    public ResponseEntity<APIResponse> getCategoryByName(@PathVariable String name){
+        try
+        {
+            Category category = categoryService.getCategoryByName(name);
+            return ResponseEntity.ok(new APIResponse("Success", category));
+        }
+        catch(ResourceNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse(e.getMessage(),null));
+        }
+    }
+
+    @DeleteMapping("/category/{id}/delete")
+    public ResponseEntity<APIResponse> deleteCategoryById(@PathVariable Long id){
+        try
+        {
+            categoryService.deleteCategoryById(id);
+            return ResponseEntity.ok(new APIResponse("Success", null));
+        }
+        catch(ResourceNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse(e.getMessage(),null));
+        }
+    }
+
+    @PutMapping("/category/{id}/update")
+    public ResponseEntity<APIResponse> UpdateCategory(@PathVariable Long id, @RequestBody Category category){
+        try
+        {
+            Category updatedCategory = categoryService.updateCategory(category, id);
+            return ResponseEntity.ok(new APIResponse("Update Success", updatedCategory));
+        }
+        catch(ResourceNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse(e.getMessage(),null));
+        }
+    }
 }
--- be/second-shops/src/main/java/com/projectcodework/second_shops/controller/ProductController.java
+++ be/second-shops/src/main/java/com/projectcodework/second_shops/controller/ProductController.java
@@ -1,4 +1,11 @@
 package com.projectcodework.second_shops.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class ProductController {
 }
